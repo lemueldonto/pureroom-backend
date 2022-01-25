72d908a78f5323a34a01dec4ede47394b6f33e7e
Lemouz-NALMOK <55319038+Lemouz-NALMOK@users.noreply.github.com>
2022-01-25 16:35:43 +0100

    Update Score

--- api/scores.js
+++ api/scores.js
@@ -9,33 +9,55 @@ const { influx: { url, token, org } } = require('../env');
 const { from, map, take }             = require('rxjs');
 
 const queryApi  = new InfluxDB({ url, token }).getQueryApi(org);
-const fluxQuery = (measurement) => `from(bucket: "ubiquarium")
+const fluxQuery = `from(bucket: "ubiquarium")
   |> range(start: -1d)
-  |> filter(fn: (r) => r["measurement"] == "${ measurement }")
+  |> filter(fn: (r) => r["measurement"] == "temperature" and r["measurement"] == "co2" and r["measurement"] == "humidity")
   |> filter(fn: (r) => r["location"] == "t1_1_ubiquarium_stand1")
   |> filter(fn: (r) => r["protocol"] == "netatmo") 
   |> yield(name: "last")`;
 
-const makeHandker = (measurement) => (req, res) => {
+function makeHan () {
     let arr   = [];
-    const sub = from(queryApi.rows(fluxQuery(measurement)))
-        .pipe(map(({ values, tableMeta }) => tableMeta.toObject(values)))
-        .subscribe({
-            next(o) {
-                arr = [ ...arr, { value: o._value, time: new Date(o._time).getTime() } ];
-            }, error(e) {
-                console.error(e);
-            }, complete() {
-                res.send(arr);
-                sub.unsubscribe();
-                // console.log('\nFinished SUCCESS');
-            },
-        });
+    return new Promise((resolve, reject) => {
+        const sub = from(queryApi.rows(fluxQuery))
+            .pipe(map(({ values, tableMeta }) => tableMeta.toObject(values)))
+            .subscribe({
+                next(o) {
+                    console.log(o);
+                    console.log ("GOOOOO")
+                }, error(e) {
+                    console.error(e);
+                    reject(e)
+                    return null;
+                }, complete() {
+                    resolve(arr);
+                    // console.log('\nFinished SUCCESS');
+                },
+            });
+    })
+
+}
+
+function formula(temp, hum, co2) {
+    return Math.floor((temp+hum+co2)/3);
+}
+
+var myLogger = async function (req, res) {
+    const arrTemp = await makeHan();
+    /*let arr = [];
+    console.log(arrTemp[23].time);
+    console.log(arrHum[23].time);
+    console.log(arrCo2[23].time);
+    for(let i = 0; i < arrTemp.length; i++) {
+        arr = [ ...arr, { value: formula(arrTemp[i].value,arrHum[i].value,arrCo2[i].value), time: arrTemp[i].time} ];
+    }
+    return arr;*/
 };
+
 
 // GET /scoring
 router.get('/scores', (req, res) => {
-    res.send([ { value: 0, time: Date.now() }]);
+    myLogger().then(data => res.send(data))
 });
 
 module.exports = router;
